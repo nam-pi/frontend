88a5e420fa4ee97837710c3dc3a52173c0bd8363
Daniel Jeller <djeller@nettek.at>
2021-05-29 13:12:39 +0000

    Add document interpretation act details

--- src/App/components/ActDetails/index.tsx
+++ src/App/components/ActDetails/index.tsx
@@ -1,29 +1,67 @@
-import { useLocaleLiteral } from "App/hooks/useLocaleLiteral";
 import { useAct } from "nampi-use-api";
-import { FormattedMessage } from "react-intl";
+import { FormattedMessage, useIntl } from "react-intl";
 import { Heading } from "../Heading";
+import { ItemLink } from "../ItemLink";
 import { LoadingPlaceholder } from "../LoadingPlaceholder";
-import { Pre } from "../Pre";
 
 interface Props {
   idLocal: string;
 }
 
 export const ActDetails = ({ idLocal }: Props) => {
-  const getText = useLocaleLiteral();
+  const { formatList, formatDate } = useIntl();
   const { data } = useAct({ idLocal });
   return data ? (
     <>
       <Heading>
         <FormattedMessage
           description="Act heading"
-          defaultMessage="Act: {label}"
-          values={{ label: getText(data.labels) }}
+          defaultMessage="Document interpretation act"
         />
       </Heading>
-      <div className="mt-4 overflow-auto">
-        <Pre>{data}</Pre>
-      </div>
+      <ul className="mt-2">
+        <li>
+          <FormattedMessage
+            description="Author text"
+            defaultMessage="{authorCount, plural, one {Author} other {Authors}}: {authors}"
+            values={{
+              authorCount: data.authors.length,
+              authors: formatList(
+                data.authors.map((a) => <ItemLink key={a.id} item={a} />),
+                { type: "conjunction" }
+              ),
+            }}
+          />
+        </li>
+        <li>
+          <FormattedMessage
+            description="Authoring date text"
+            defaultMessage="Authoring date: {date}"
+            values={{
+              date: formatDate(data.date, { dateStyle: "long" }),
+            }}
+          />
+        </li>
+        <li>
+          <FormattedMessage
+            description="Source text"
+            defaultMessage="Source: {source}, {location}"
+            values={{
+              source: <ItemLink item={data.sourceLocation.source} />,
+              location: data.sourceLocation.text,
+            }}
+          />
+        </li>
+        <li>
+          <FormattedMessage
+            description="Interpretation text"
+            defaultMessage="Interpretation: {interpretation}"
+            values={{
+              interpretation: <ItemLink item={data.interpretation} />,
+            }}
+          />
+        </li>
+      </ul>
     </>
   ) : (
     <LoadingPlaceholder />
