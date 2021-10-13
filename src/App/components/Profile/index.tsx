38c19a552cdcae0c036862ea7ea7ed66a96d4db3
Daniel Jeller <djeller@nettek.at>
2021-10-13 23:10:00 +0200

    Fill in profile page content

--- src/App/components/Profile/index.tsx
+++ src/App/components/Profile/index.tsx
@@ -1,22 +1,46 @@
+import { faLink } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useLocaleLiteral } from "App/hooks/useLocaleLiteral";
 import { useUser } from "nampi-use-api";
 import { FormattedMessage } from "react-intl";
+import { Link } from "react-router-dom";
 import { Heading } from "../Heading";
 import { LoadingPlaceholder } from "../LoadingPlaceholder";
+import { Paragraph } from "../Paragraph";
 
 export const Profile = () => {
   const { initialized, loading, data } = useUser();
+  const literal = useLocaleLiteral();
   return (
-    <div>
+    <div className="space-y-4">
       <Heading>
         <FormattedMessage
-          description="Profile page label"
-          defaultMessage="Profile"
+          description="Profile page heading"
+          defaultMessage="Welcome, {user}"
+          values={{ user: data?.username }}
         />
       </Heading>
       {!initialized || loading ? (
         <LoadingPlaceholder />
       ) : (
-        <p>{JSON.stringify(data)}</p>
+        <Paragraph>
+          <FormattedMessage
+            description="Profile page text"
+            defaultMessage="Your email address is {email} and your author page is {author}."
+            values={{
+              email: <span className="text-gray-500">{data?.email}</span>,
+              author: (
+                <Link className="text-gray-500" to={data?.author?.id || ""}>
+                  {literal(data?.labels)}
+                  <FontAwesomeIcon
+                    className="text-blue-500 text-xs ml-1"
+                    icon={faLink}
+                  />
+                </Link>
+              ),
+            }}
+          />
+        </Paragraph>
       )}
     </div>
   );
